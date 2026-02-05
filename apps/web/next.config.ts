7c3ec285039ab43950634be7f457a92b348a5721
monakecil <your-email@example.com>
2026-02-06 02:04:36 +0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@chat-read/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
